6861f700bc4836dbba78030845b62c07ad23881c
Haoyue Sun <eclipse_ss@outlook.com>
2024-07-10 14:15:58 +1000

    1

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -17,7 +17,10 @@ def loop_ranger(start, stop=None, step=1):
     Look up for how range() works in the python docs. You could  answer this
     with just the range function, but we'd like you to do it the long way.
     """
-    return None
+    new_list=[]
+    for i in range(start,stop,step):
+        new_list.append(i)
+    return new_list
 
 
 def two_step_ranger(start, stop):
@@ -28,7 +31,8 @@ def two_step_ranger(start, stop):
 
     You can either reuse loop_ranger, or the range function that in the standard library
     """
-    return None
+    return list(range(start,stop,2))
+
 
 
 def stubborn_asker(low, high):
@@ -39,8 +43,12 @@ def stubborn_asker(low, high):
 
     Look up the docs for a function called "input"
     """
-    return None
-
+    while True:
+        input_number= int(input())
+        if low <= input_number <=high:
+            return input_number
+        else:
+            print ("again")
 
 def not_number_rejector(message):
     """Ask for a number repeatedly until actually given one.
@@ -49,7 +57,14 @@ def not_number_rejector(message):
     (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    return None
+    while True:
+        try:
+            input_number= int(input(message))
+            print(input_number)
+            return input_number
+        except Exception as e:
+            print('again'.format(e))
+
 
 
 def super_asker(low, high):
@@ -58,7 +73,17 @@ def super_asker(low, high):
     Combine what you learnt from stubborn_asker and not_number_rejector
     to make a function that does it all!
     """
-    return None
+    while True:
+        try:
+            input_number= int(input())
+            print(input_number)
+        except Exception as e:
+            print('again'.format(e))            
+        if low <= input_number <=high:
+            return input_number
+
+
+        
 
 
 if __name__ == "__main__":
